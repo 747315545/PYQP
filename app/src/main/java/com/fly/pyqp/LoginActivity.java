5a115a77c555055e3257195ec39152eb7798ad55
unknown <huangfei@wind.local>
2017-09-25 18:37:44 +0800

    登录注册界面完善
     Changes to be committed:
    	modified:   app/src/main/java/com/fly/pyqp/LoginActivity.java
    	modified:   app/src/main/java/com/fly/pyqp/fragment/RegistFragment.java
    	new file:   app/src/main/res/drawable/bg_button_clicked.xml
    	modified:   app/src/main/res/drawable/bg_button_normal.xml
    	modified:   app/src/main/res/drawable/bg_editview_normal.xml
    	new file:   app/src/main/res/drawable/btn_getcode_clicked.xml
    	new file:   app/src/main/res/drawable/btn_getcode_normal.xml
    	new file:   app/src/main/res/drawable/button_style.xml
    	new file:   app/src/main/res/drawable/jiantou.png
    	new file:   app/src/main/res/drawable/qq_clicked.png
    	new file:   app/src/main/res/drawable/qq_icon.xml
    	new file:   app/src/main/res/drawable/qq_normal.png
    	new file:   app/src/main/res/drawable/text_style.xml
    	new file:   app/src/main/res/drawable/weibo_clicked.png
    	new file:   app/src/main/res/drawable/weibo_icon.xml
    	new file:   app/src/main/res/drawable/weibo_normal.png
    	new file:   app/src/main/res/drawable/weixin_clicked.png
    	new file:   app/src/main/res/drawable/weixin_icon.xml
    	new file:   app/src/main/res/drawable/weixin_normal.png
    	modified:   app/src/main/res/layout/activity_login.xml
    	modified:   app/src/main/res/layout/fragment_login.xml
    	modified:   app/src/main/res/layout/fragment_regist.xml
    	new file:   app/src/main/res/values-w820dp/dimens.xml
    	modified:   app/src/main/res/values/colors.xml
    	new file:   app/src/main/res/values/dimens.xml
    	modified:   app/src/main/res/values/strings.xml
    	modified:   app/src/main/res/values/styles.xml

--- app/src/main/java/com/fly/pyqp/LoginActivity.java
+++ app/src/main/java/com/fly/pyqp/LoginActivity.java
@@ -6,8 +6,11 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.fly.pyqp.fragment.LoginFragment;
 import com.fly.pyqp.fragment.RegistFragment;
@@ -15,8 +18,10 @@ import com.fly.pyqp.fragment.RegistFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoginActivity extends FragmentActivity {
+public class LoginActivity extends FragmentActivity implements View.OnClickListener{
     private ViewPager viewPager;
+    private ImageView imageView_login;
+    private ImageView imageView_regist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,11 @@ public class LoginActivity extends FragmentActivity {
         init();
     }
 
-    private void init(){
+    private void init() {
+        imageView_login = findViewById(R.id.img_login_jt);
+        imageView_regist = findViewById(R.id.img_regist_jt);
+        findViewById(R.id.btn_login_title).setOnClickListener(this);
+        findViewById(R.id.btn_regist_title).setOnClickListener(this);
         viewPager = findViewById(R.id.vp_login);
         final List<Fragment> fragments = new ArrayList<>();
         fragments.add(new LoginFragment());
@@ -46,5 +55,52 @@ public class LoginActivity extends FragmentActivity {
                 return fragments.size();
             }
         });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        imageView_login.setVisibility(View.VISIBLE);
+                        imageView_regist.setVisibility(View.INVISIBLE);
+                        imageView_login.setAnimation(AnimationUtils.makeInAnimation(LoginActivity.this, true));
+                        imageView_regist.setAnimation(AnimationUtils.makeOutAnimation(LoginActivity.this, true));
+                        break;
+                    case 1:
+                        imageView_login.setVisibility(View.INVISIBLE);
+                        imageView_regist.setVisibility(View.VISIBLE);
+                        imageView_login.setAnimation(AnimationUtils.makeOutAnimation(LoginActivity.this, false));
+                        imageView_regist.setAnimation(AnimationUtils.makeInAnimation(LoginActivity.this, false));
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_login_title:
+                viewPager.setCurrentItem(0,true);
+                break;
+            case R.id.btn_regist_title:
+                viewPager.setCurrentItem(1,true);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(false);
     }
 }
--- app/src/main/java/com/fly/pyqp/fragment/RegistFragment.java
+++ app/src/main/java/com/fly/pyqp/fragment/RegistFragment.java
@@ -1,11 +1,14 @@
 package com.fly.pyqp.fragment;
 
+import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -21,6 +24,7 @@ public class RegistFragment extends Fragment implements View.OnClickListener{
     private EditText regist_name;
     private EditText regist_password;
     private EditText regist_code;
+    private Button btn_getcode;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -33,7 +37,8 @@ public class RegistFragment extends Fragment implements View.OnClickListener{
         regist_name = view.findViewById(R.id.edit_regist_name);
         regist_password = view.findViewById(R.id.edit_regist_password);
         regist_code = view.findViewById(R.id.edit_regist_code);
-        view.findViewById(R.id.btn_getcode).setOnClickListener(this);
+        btn_getcode = view.findViewById(R.id.btn_getcode);
+        btn_getcode.setOnClickListener(this);
         view.findViewById(R.id.btn_regist).setOnClickListener(this);
         ((CheckBox)view.findViewById(R.id.cb_agree)).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -47,9 +52,34 @@ public class RegistFragment extends Fragment implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_getcode:
+                TimeCount timeCount = new TimeCount(61100,1000);
+                timeCount.start();
                 break;
             case R.id.btn_regist:
                 break;
         }
     }
+
+    class TimeCount extends CountDownTimer {
+
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            btn_getcode.setBackground(getActivity().getResources().getDrawable(R.drawable.btn_getcode_clicked));
+            btn_getcode.setClickable(false);
+            btn_getcode.setText("("+((millisUntilFinished / 1000)-1) +") 秒");
+        }
+
+        @Override
+        public void onFinish() {
+            btn_getcode.setText("重新获取验证码");
+            btn_getcode.setClickable(true);
+            btn_getcode.setBackground(getActivity().getResources().getDrawable(R.drawable.btn_getcode_normal));
+
+        }
+    }
+
 }
